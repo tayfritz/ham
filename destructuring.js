5529bef925d1c8c582a6e82d1a3289a96ce536ec
tayfritz01@gmail.com <taylorfritz@Taylors-Air.attlocal.net>
2021-06-30 17:05:24 -0400

    Reorganization of files

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+const jsonData = [
+    {"cduAccessCode":"AECB3AEC","ufcAccessCode":"3PI1E42I"},{"cduAccessCode":"AECB3AEC","ufcAccessCode":"5I1KQGHN"},{"cduAccessCode":"AECB3AEC","ufcAccessCode":"IB1D1V1E"}
+]
+
+const practice = [];
+
+for (const {cduAccessCode, ufcAccessCode} of jsonData) {
+    
+}
+
+jsonData.forEach( () => {
+    practice.push(cduAccessCode);
+})
+
+console.log(pratice);
